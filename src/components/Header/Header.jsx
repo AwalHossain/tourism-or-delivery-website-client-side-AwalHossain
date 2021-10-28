ae175ddb98bddbbaa730be25e0fa6b4c2a5ff9dc
AwalHossain <waqin21@gmail.com>
2021-10-29 00:04:23 +0600

    navbar included for the very first time

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="ms-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#features">Features</Nav.Link>
+                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                </Nav>
+                </Navbar.Collapse>
+
+                </Container>
+        </Navbar>
+        </div>
+    );
+};
+
+export default Header;
